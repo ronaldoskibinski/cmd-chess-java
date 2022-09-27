b81a1815f9d1d0648ee3e034dfbba084a1cb9604
Ronaldo Skibinski <ronaldoskibinski@outlook.com>
2022-09-27 19:40:32 -0300

    Print the chess board and create some chess pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,14 @@
 package application;
 
-import board.Board;
-import board.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Position pos = new Position(3, 5);
-		// instantiating a board
-		Board board = new Board(8, 8);
+
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
