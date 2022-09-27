8f079e1f3f941589d29ee072b19d9f6670f599bc
Ronaldo Skibinski <ronaldoskibinski@outlook.com>
2022-09-26 21:22:36 -0300

    Implementing a Board packagen with Board, Piece and Position Classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import board.Board;
+import board.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		Position pos = new Position(3, 5);
+		// instantiating a board
+		Board board = new Board(8, 8);
 	}
 
 }
